6f6e1eb54acc79efee8802ad5aa35d6424bb377c
hector Martinez <hmartinezmoreira81@gmail.com>
2023-11-26 18:04:06 -0600

    first commit

---
+++ src/main/java/website/pelsmi11/security/SecurityApplication.java
@@ -0,0 +1,13 @@
+package website.pelsmi11.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/website/pelsmi11/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package website.pelsmi11.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
